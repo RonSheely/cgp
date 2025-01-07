9d8cf040ac9373284d7f19c1cecc9ccbf937edde
Soares Chen <soares.chen@maybevoid.com>
2025-01-07 15:42:42 +0100

    Implement `ErrorWrapper` on generic `ErrorRaiser` providers (#54)
    * Implement ErrorWrapper together with ErrorRaiser providers
    
    * Implement ErrorWrapper for BoxedStdError
    
    * Add changelog

--- crates/cgp-error-anyhow/src/lib.rs
+++ crates/cgp-error-anyhow/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+extern crate alloc;
+
 mod impls;
 
+pub use anyhow::Error;
 pub use impls::{DebugAnyhowError, DisplayAnyhowError, RaiseAnyhowError, UseAnyhowError};
--- crates/cgp-error-eyre/src/lib.rs
+++ crates/cgp-error-eyre/src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
 
+extern crate alloc;
+
 mod impls;
 
+pub use eyre::Error;
 pub use impls::{DebugEyreError, DisplayEyreError, RaiseEyreError, UseEyreError};
