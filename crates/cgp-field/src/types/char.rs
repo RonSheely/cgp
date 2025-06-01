a57245fb2210ce37b52470e8b4b263bd249181e3
Soares Chen <soares.chen@maybevoid.com>
2025-06-01 22:37:51 +0200

    Format `Char` statically without `self` (#104)
    * Add StaticFormat trait
    
    * Make Char field PhantomData

--- crates/cgp-field/src/types/char.rs
+++ crates/cgp-field/src/types/char.rs
@@ -1,16 +1,28 @@
 use core::fmt::Display;
+use core::marker::PhantomData;
 
 #[derive(Eq, PartialEq, Clone, Copy, Default)]
-pub struct ι<const CHAR: char, Tail>(pub Tail);
+pub struct ι<const CHAR: char, Tail>(pub PhantomData<Tail>);
 
 pub use ι as Char;
 
+use crate::StaticFormat;
+
 impl<const CHAR: char, Tail> Display for Char<CHAR, Tail>
 where
-    Tail: Display,
+    Self: StaticFormat,
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        <Self as StaticFormat>::fmt(f)
+    }
+}
+
+impl<const CHAR: char, Tail> StaticFormat for Char<CHAR, Tail>
+where
+    Tail: StaticFormat,
+{
+    fn fmt(f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{CHAR}")?;
-        self.0.fmt(f)
+        Tail::fmt(f)
     }
 }
