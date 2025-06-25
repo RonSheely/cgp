8931d052c23e6c3b3a38b1b06350b985a0511e2d
Soares Chen <soares.chen@maybevoid.com>
2025-06-25 23:59:57 +0200

    Implement `DispatchHandlersRef` for dispatching based on enum references (#116)
    * Implement DispatchHandlersRef for dispatching references
    
    * Implement DispatchFieldsRef
    
    * Implement DispatchFieldsRef for TryComputer and Handler
    
    * Make argument to HandleFieldValue explicit
    
    * Add separate DispatchFieldValues type aliases
    
    * Fix clippy

--- crates/cgp-dispatch/src/providers/dispatch_fields.rs
+++ crates/cgp-dispatch/src/providers/dispatch_fields.rs
@@ -1,9 +1,18 @@
 use cgp_core::prelude::*;
-use cgp_handler::{Computer, ComputerComponent, Handler, HandlerComponent};
+use cgp_handler::{
+    Computer, ComputerComponent, HandleFieldValue, Handler, HandlerComponent, TryComputer,
+    TryComputerComponent,
+};
 
-use crate::{DispatchHandlers, ExtractFieldAndHandle};
+use crate::{DispatchHandlers, DispatchHandlersRef, ExtractFieldAndHandle};
 
 pub struct DispatchFields<Provider = UseContext>(pub PhantomData<Provider>);
+
+pub struct DispatchFieldsRef<Provider = UseContext>(pub PhantomData<Provider>);
+
+pub type DispatchFieldValues = DispatchFields<HandleFieldValue<UseContext>>;
+
+pub type DispatchFieldValueRefs = DispatchFieldsRef<HandleFieldValue<UseContext>>;
 
 #[cgp_provider]
 impl<Context, Code, Input, Output, Fields, Provider> Computer<Context, Code, Input>
@@ -17,6 +26,26 @@ where
 
     fn compute(context: &Context, code: PhantomData<Code>, input: Input) -> Output {
         DispatchHandlers::compute(context, code, input)
+    }
+}
+
+#[cgp_provider]
+impl<Context, Code, Input, Output, Fields, Provider> TryComputer<Context, Code, Input>
+    for DispatchFields<Provider>
+where
+    Context: HasErrorType,
+    Input: HasFields<Fields = Fields>,
+    Fields: FieldsToExtractFieldHandlers<Provider>,
+    DispatchHandlers<Fields::Handlers>: TryComputer<Context, Code, Input, Output = Output>,
+{
+    type Output = Output;
+
+    fn try_compute(
+        context: &Context,
+        code: PhantomData<Code>,
+        input: Input,
+    ) -> Result<Output, Context::Error> {
+        DispatchHandlers::try_compute(context, code, input)
     }
 }
 
@@ -37,6 +66,64 @@ where
         input: Input,
     ) -> Result<Output, Context::Error> {
         DispatchHandlers::handle(context, code, input).await
+    }
+}
+
+#[cgp_provider]
+impl<Context, Code, Input, Output, Provider> Computer<Context, Code, &Input>
+    for DispatchFieldsRef<Provider>
+where
+    Input: HasFieldsRef,
+    for<'b> Input::FieldsRef<'b>: FieldsToExtractFieldHandlers<Provider>,
+    for<'b> DispatchHandlersRef<<Input::FieldsRef<'b> as FieldsToExtractFieldHandlers<Provider>>::Handlers>:
+        Computer<Context, Code, &'b Input, Output = Output>,
+{
+    type Output = Output;
+
+    fn compute(context: &Context, code: PhantomData<Code>, input: &Input) -> Output {
+        DispatchHandlersRef::compute(context, code, input)
+    }
+}
+
+#[cgp_provider]
+impl<Context, Code, Input, Output, Provider> TryComputer<Context, Code, &Input>
+    for DispatchFieldsRef<Provider>
+where
+    Context: HasErrorType,
+    Input: HasFieldsRef,
+    for<'b> Input::FieldsRef<'b>: FieldsToExtractFieldHandlers<Provider>,
+    for<'b> DispatchHandlersRef<<Input::FieldsRef<'b> as FieldsToExtractFieldHandlers<Provider>>::Handlers>:
+        TryComputer<Context, Code, &'b Input, Output = Output>,
+{
+    type Output = Output;
+
+    fn try_compute(
+        context: &Context,
+        code: PhantomData<Code>,
+        input: &Input,
+    ) -> Result<Output, Context::Error> {
+        DispatchHandlersRef::try_compute(context, code, input)
+    }
+}
+
+#[cgp_provider]
+impl<Context, Code: Send, Input, Output: Send, Provider> Handler<Context, Code, &Input>
+    for DispatchFieldsRef<Provider>
+where
+    Context: HasAsyncErrorType,
+    Input: Send + Sync + HasFieldsRef,
+    for<'b> Input::FieldsRef<'b>: FieldsToExtractFieldHandlers<Provider>,
+    for<'b> DispatchHandlersRef<<Input::FieldsRef<'b> as FieldsToExtractFieldHandlers<Provider>>::Handlers>:
+        Handler<Context, Code, &'b Input, Output = Output>,
+{
+    type Output = Output;
+
+    async fn handle(
+        context: &Context,
+        code: PhantomData<Code>,
+        input: &Input,
+    ) -> Result<Output, Context::Error> {
+        DispatchHandlersRef::handle(context, code, input).await
     }
 }
 
