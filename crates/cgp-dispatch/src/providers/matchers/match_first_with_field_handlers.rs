a02478190dc3bbf543f1b26049d2d1b78206a663
Soares Chen <soares.chen@maybevoid.com>
2025-08-05 21:31:15 +0200

    Add extensible visitor support mutable partial variants (#135)
    * Refactor MapTypeRef
    
    * Add MapTypeRef parameter to partial-ref variants
    
    * Fix deriving partial-ref variants for enums containing generics
    
    * Derive HasExtractorMut
    
    * Refactor ToFieldHandlers
    
    * Add HasExtractorAnyRef
    
    * Implement MatchFirstWithHandlersMut
    
    * Test use of MatchFirstWithValueHandlersMut

--- crates/cgp-dispatch/src/providers/matchers/match_first_with_field_handlers.rs
+++ crates/cgp-dispatch/src/providers/matchers/match_first_with_field_handlers.rs
@@ -1,8 +1,13 @@
 use cgp_core::prelude::*;
 use cgp_handler::UseInputDelegate;
 
-use crate::providers::matchers::to_first_field_handlers::HasFirstFieldHandlers;
-use crate::{HandleFirstFieldValue, MatchFirstWithHandlers, MatchFirstWithHandlersRef};
+use crate::providers::matchers::to_field_handlers::{
+    HasFieldHandlers, MapExtractFirstFieldAndHandle,
+};
+use crate::{
+    HandleFirstFieldValue, MatchFirstWithHandlers, MatchFirstWithHandlersMut,
+    MatchFirstWithHandlersRef,
+};
 
 pub type MatchFirstWithFieldHandlers<Provider = UseContext> =
     UseInputDelegate<MatchFirstWithFieldHandlersInputs<Provider>>;
@@ -16,17 +21,31 @@ pub type MatchFirstWithFieldHandlersRef<Provider = UseContext> =
 pub type MatchFirstWithValueHandlersRef<Provider = UseContext> =
     UseInputDelegate<MatchFirstWithFieldHandlersInputsRef<HandleFirstFieldValue<Provider>>>;
 
+pub type MatchFirstWithFieldHandlersMut<Provider = UseContext> =
+    UseInputDelegate<MatchFirstWithFieldHandlersInputsMut<Provider>>;
+
+pub type MatchFirstWithValueHandlersMut<Provider = UseContext> =
+    UseInputDelegate<MatchFirstWithFieldHandlersInputsMut<HandleFirstFieldValue<Provider>>>;
+
 delegate_components! {
-    <Input: HasFirstFieldHandlers<Provider>, Args, Provider>
+    <Input: HasFieldHandlers<MapExtractFirstFieldAndHandle<Provider>>, Args, Provider>
     new MatchFirstWithFieldHandlersInputs<Provider> {
         (Input, Args): MatchFirstWithHandlers<Input::Handlers>
     }
 }
 
 delegate_components! {
-    <Input: HasFirstFieldHandlers<Provider>, Args, Provider>
+    <Input: HasFieldHandlers<MapExtractFirstFieldAndHandle<Provider>>, Args, Provider>
     new MatchFirstWithFieldHandlersInputsRef<Provider> {
         <'a> (&'a Input, Args):
             MatchFirstWithHandlersRef<Input::Handlers>
+    }
+}
+
+delegate_components! {
+    <Input: HasFieldHandlers<MapExtractFirstFieldAndHandle<Provider>>, Args, Provider>
+    new MatchFirstWithFieldHandlersInputsMut<Provider> {
+        <'a> (&'a mut Input, Args):
+            MatchFirstWithHandlersMut<Input::Handlers>
     }
 }
--- crates/cgp-macro-lib/src/entrypoints/derive_extract_field.rs
+++ crates/cgp-macro-lib/src/entrypoints/derive_extract_field.rs
@@ -3,8 +3,9 @@ use quote::quote;
 use syn::{parse2, Ident, ItemEnum};
 
 use crate::derive_extractor::{
-    derive_extract_field_impls, derive_extractor_enum, derive_finalize_extract_impl,
-    derive_has_extractor_impl, derive_has_extractor_ref_impl,
+    derive_extract_field_impls, derive_extractor_enum, derive_extractor_enum_ref,
+    derive_finalize_extract_impl, derive_has_extractor_impl, derive_has_extractor_mut_impl,
+    derive_has_extractor_ref_impl,
 };
 
 pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
@@ -13,18 +14,23 @@ pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
     let context_ident = &context_enum.ident;
 
     let extractor_ident = Ident::new(&format!("Partial{context_ident}"), context_ident.span());
-    let extractor_enum = derive_extractor_enum(&context_enum, &extractor_ident, false)?;
+    let extractor_enum = derive_extractor_enum(&context_enum, &extractor_ident)?;
 
     let extractor_ref_ident =
         Ident::new(&format!("PartialRef{context_ident}"), context_ident.span());
-    let extractor_ref_enum = derive_extractor_enum(&context_enum, &extractor_ref_ident, true)?;
+    let extractor_ref_enum = derive_extractor_enum_ref(&context_enum, &extractor_ref_ident)?;
 
     let has_extractor_impl = derive_has_extractor_impl(&context_enum, &extractor_ident)?;
+
     let has_extractor_ref_impl =
         derive_has_extractor_ref_impl(&context_enum, &extractor_ref_ident)?;
 
+    let has_extractor_mut_impl =
+        derive_has_extractor_mut_impl(&context_enum, &extractor_ref_ident)?;
+
     let finalize_extract_impl =
         derive_finalize_extract_impl(&context_enum, &extractor_ident, false)?;
+
     let finalize_extract_ref_impl =
         derive_finalize_extract_impl(&context_enum, &extractor_ref_ident, true)?;
 
@@ -38,6 +44,7 @@ pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
 
         #has_extractor_impl
         #has_extractor_ref_impl
+        #has_extractor_mut_impl
 
         #finalize_extract_impl
         #finalize_extract_ref_impl
